e83bbf2234edad68067a937d38c2cba230ae6abd
Icecream95 <the.real.icecream95@gmail.com>
2017-08-14 20:31:45 +1200

    Various changes

--- src/llscratch.cpp
+++ src/llscratch.cpp
@@ -13,28 +13,44 @@ void usage(std::string pn)
     // TODO: // std::cerr << "Usage: " << pn << " SOURCE [DEST]\n\nIf DEST is missing, will output to SOURCE, replacing .ll with .json\nTo read/write using standard input, use - for SOURCE and/or DEST\n\n";
 }
 
-std::string convert(std::istream& ifs)
-{
-    char ch = '\0';
-    while (true)
+namespace Convert {
+    std::string funct_define(std::istream& ifs)
     {
-        ch = ifs.peek();
-        if (ch == ';')
-        {
-            ifs.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            continue;
-        }
-        std::string s;
-        ifs >> s;
-        static const std::set<std::string> ignore
-            {"source_filename","target","attributes"};
-        if (ignore.count(s))
-        {
-            ifs.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
-            continue;
-        }
+        return "";
     }
-    return std::string{(std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>())};
+
+    std::string convert(std::istream& ifs)
+    {
+        static const std::string start_boilerplate
+            {""}; // TODO
+        std::string output {start_boilerplate};
+        char ch = '\0';
+        while (true)
+        {
+            ch = ifs.peek();
+            if (ch == ';' || ch == '!')
+            {
+                ifs.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                continue;
+            }
+            std::string s;
+            ifs >> s;
+             static const std::set<std::string> ignore
+                {"source_filename","target","attributes"};
+            if (ignore.count(s))
+            {
+                ifs.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+                continue;
+            }
+            if (s == "define")
+                output += funct_define(ifs);
+        }
+        static const std::string end_boilerplate
+            {""}; // TODO
+        output += end_boilerplate;
+        return output;
+        // return std::string{(std::istreambuf_iterator<char>(ifs)), (std::istreambuf_iterator<char>())};
+    }
 }
 
 int main(int argc, char* argv[])
@@ -46,5 +62,5 @@ int main(int argc, char* argv[])
     }
     std::ifstream ifs {argv[1]};
     std::ofstream ofs {argv[2]};
-    ofs << convert(ifs);
+    ofs << Convert::convert(ifs);
 }
