fa957825d5f95fcd57c17561ab42e62e6979b36c
Icecream95 <the.real.icecream95@gmail.com>
2017-08-18 14:06:27 +1200

    Various changes including code cleanup and parsing code

--- src/llscratch.cpp
+++ src/llscratch.cpp
@@ -6,6 +6,7 @@
 #include <map>
 #include <set>
 #include <limits>
+#include <utility>
 
 void usage(std::string pn)
 {
@@ -15,16 +16,27 @@ void usage(std::string pn)
 
 namespace Convert {
 
+    inline std::string without_last(const std::string& s)
+    {
+        return s.substr(0,s.size()-1);
+    }
+
+    inline void strip_last(std::string& s)
+    {
+        s = without_last(s);
+    }
+
+    inline void ignore_until(std::istream& ifs, const char ch)
+    {   // This is just a helper function to cut down on repeated code
+        ifs.ignore(std::numeric_limits<std::streamsize>::max(),ch);
+    }
+
+
     std::string parse_code(std::istream& ifs)
     {
         std::stringstream s;
         s << ifs.rdbuf();
         return s.str();
-    }
-
-    void ignore_until(std::istream& ifs, const char ch)
-    {   // This is just a helper function to cut down on repeated code
-        ifs.ignore(std::numeric_limits<std::streamsize>::max(),ch);
     }
 
     std::string get_block(std::istream& ifs, const char open, const char close, int depth = 0)
@@ -54,17 +66,54 @@ namespace Convert {
         while ((to_parse.get(ctmp), ctmp!='(') && (name += ctmp, true));
         // The function name is in name
         std::string args = get_block(to_parse,'(',')',1);
-        args = args.substr(0,args.size()-1);
+        strip_last(args);
         // The arguments the function takes is in args
+
+        // Argument parser
+        int argc = 0;
+        std::vector<std::pair<std::string,std::pair<std::string,std::string>>> arguments;
+        std::string tmpdesc;
+        std::string tmptype;
+        bool type_yet {false};
+        std::stringstream argstream {args};
+        while (argstream >> tmp)
+            if (tmp[0] == '%') {
+                arguments.push_back(std::make_pair(tmp.substr(1,tmp.size()-1),std::make_pair(tmptype,tmpdesc)));
+                tmpdesc = "";
+                tmptype = "";
+                type_yet = false;
+                ++argc;
+            }
+            else if (type_yet)
+                tmpdesc += tmp + ' ';
+            else {
+                type_yet = true;
+                tmptype = tmp;
+            }
+        tmp = "";
+        for (auto x: arguments)
+            tmp += "\"~" + name + "~" + x.second.first + "~" + x.first + "\",";
+
+        // Scratch json for defining a function:
+        // [0,0,[["procDef", "funcname %s %s %s", ["argname1", "argname2", "argname3"], ["", "", ""], true], <code here>
+        output = "[0,0,[\"procDef\", \""+name+" \", [" + without_last(tmp) + "], [";
+        for (int i = 0; i < argc; ++i)
+            output += "\"\","; // This is where default arguments go
+        strip_last(output);
+        output += "], true]]]";
+        // Should we store numbers as numbers, or as strings?
+        // The latter would make parsing easier but a slight loss of performance may be seen. I don't think the performance difference would be too bad, as it only impacts function calls
+
         ignore_until(to_parse,'{');
         std::string body = get_block(to_parse,'{','}',1);
-        body = body.substr(0,body.size()-1);
+        strip_last(body);
         // The function body is in body
 
         std::stringstream body_s {body};
         ignore_until(body_s,':');
 
-        return "Class:\nName: " + name + "\nArguments: " + args + "\nBody: " + parse_code(body_s) + '\n';
+        return output;
+        // return "Class:\nName: " + name + "\nArguments: " + args + "\nBody: " + parse_code(body_s) + '\n';
     }
 
     std::string convert(std::istream& ifs)
