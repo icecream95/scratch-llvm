0c95bc40e34f66f1571c9f416cd8ea1c73f9ed7b
Icecream95 <the.real.icecream95@gmail.com>
2017-08-19 19:59:31 +1200

    Started on new llscratch.cpp

--- src/llscratch.cpp
+++ src/llscratch.cpp
@@ -1,1 +1,21 @@
-int main() {}
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <map>
+#include <set>
+#include <limits>
+#include <utility>
+#include <functional>
+
+using namespace std;
+
+struct Syntax_table_function :std::function<string(istream&,map<string,Syntax_table_function>&)> {/* Empty body */}; // A CRTP (https://en.wikipedia.org/wiki/Curiously_recurring_template_pattern)
+// Should map be hardcoded in?
+
+using Syntax_map = map<string,Syntax_table_function>;
+
+int main(int argc, char* argv[])
+{
+    Syntax_map syntax_table;
+}
