032880f7396543a17934fb3395aeb2dbf9cce9f1
Icecream95 <the.real.icecream95@gmail.com>
2017-08-19 20:36:46 +1200

    Started work on helper class which can unify multiple istreams.
    This is so the string already extracted for syntax item identification can be passed as a stringstream unified with the istream from the file, which is needed for further parsing.

--- src/llscratch.cpp
+++ src/llscratch.cpp
@@ -8,11 +8,18 @@
 #include <utility>
 #include <functional>
 
-struct Syntax_table_function :public std::function<std::string(std::istream&,std::map<std::string,Syntax_table_function>&)> {/* Empty body */};
+struct Syntax_table_function :public std::function<std::string(std::istream&,std::map<std::string,Syntax_table_function>&)> {};
 // A CRTP (https://en.wikipedia.org/wiki/Curiously_recurring_template_pattern)
 // Should map be hardcoded in?
 
 using Syntax_map = std::map<std::string,Syntax_table_function>;
+
+template <typename If, typename Is, typename T, typename Tr = std::char_traits<T>>
+struct Basic_multi_istream {
+};
+
+template <typename If, typename Is>
+struct Multi_istream :Basic_multi_istream<If,Is,char> {};
 
 int main(int argc, char* argv[])
 {
