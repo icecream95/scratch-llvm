afe3d6f9bcf65002d473e62b7efb2239dcf17e71
Icecream95 <the.real.icecream95@gmail.com>
2017-08-24 11:25:02 +1200

    Work on Basic_parse_string

--- src/include/scratch-llvm/parse_map.hpp
+++ src/include/scratch-llvm/parse_map.hpp
@@ -23,20 +23,45 @@ public:
     }
     bool eq(const Basic_parse_string<C,Tr,A>& rhs) const
     {
+        // Should this have an out of class equivalent?
+
         // This is faster than operator==, but doesn't do any parsing,
         //     so Parse_string{"Hello, \\w"} == "Hello, World!",
         //     but !(Parse_string{"Hello, \\w"}.eq("Hello, World!"))
         return (blk == rhs.blk)?true:false;
     }
+    bool less(const Basic_parse_string<C,Tr,A>& rhs) const
+    {
+        // This is similar to eq: it is faster than operator<,
+        //     but is just a shallow less and doesn't do any parsing
+        return (blk.size()<rhs.blk.size() || blk<rhs.blk)?true:false;
+    }
     bool operator==(const Basic_parse_string<C,Tr,A>& rhs) const
     {
-        int ind1 = 0;
-        int ind2 = 0;
-        for (;;) {
+        // We could optimise this by having a one-sided equals
+        //     (i.e. a==b && !(b==a) could be true) but that
+        //     would be confusing, and could lead to obscure errors
+        int i1 = 0;
+        int i2 = 0;
+        while (i1!=blk.size() && i2!=rhs.blk.size()) {
+            if (blk[i1] == rhs.blk[i2]) {
+                ++i1;
+                ++i2;
+                continue;
+            }
+            if (blk[i1].first == rhs.blk[i2].first)
+                return false;
+            
         }
     }
     bool operator<(const Basic_parse_string<C,Tr,A>& rhs) const
     {
+        // This needs to be modified so that
+        //        (a==b) == !(a<b || b<a)
+        //     holds true. Currently, it is only
+        //         a.eq(b) == !(a<b || b<a)
+        //     that is always true.
+        // We need to do this as otherwise lookup in a map may fail.
         return (blk.size()<rhs.blk.size() || blk<rhs.blk)?true:false;
     }
 private:
